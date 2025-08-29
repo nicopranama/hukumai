d95f924ab993b7b1db6ebd11318e6248fb7ab57f
nicopranama <nicopranama19@gmail.com>
2025-08-29 19:37:26 +0700

    add embed.py: complete embedding functions

--- embed.py
+++ embed.py
@@ -5,6 +5,7 @@ import requests
 import fitz
 from chromadb.utils import embedding_functions
 import chromadb
+from dotenv import load_dotenv
 
 def pdf_to_text(url):
     try:
@@ -43,3 +44,45 @@ def split_text_into_sections(text, min_chars_per_section):
         sections.append(current_section.strip())
     
     return sections
+
+def embed_text_in_chromadb(text, document_name, document_description, persist_directory=Utils.DB_FOLDER):
+    openai_ef = embedding_functions.OpenAIEmbeddingFunction(
+        load_dotenv(),
+        api_key = OS.getenv("OPENAI_API_KEY"),
+        model_name = "text-embedding-ada-002"
+    )
+    documents = split_text_into_sections(text, 1000)
+
+    ids = [str(hash(d)) for d in documents]
+
+    metadata = {
+        "name": document_name,
+        "description": document_description
+    }
+    metadatas = [metadata] * len(documents)
+
+    client = chromadb.PersistentClient(path=persist_directory)
+    collection_name = 'collection_1'
+    collection = client.get_or_create_collection(name=collection_name, embedding_function=openai_ef)
+
+    count = collection.count()
+    print(f"Collection already contains {count} documents")
+    ids = [str(i) for i in range(count, count + len(documents))]
+
+    for i in tqdm(
+        range(0, len(documents), 100), desc="Adding documents", unit_scale=100
+    ):
+        collection.add(
+            ids=ids[i:i+100],
+            documents=documents[i:i+100],
+            metadatas=metadatas[i:i+100]
+        )
+    
+    new_count = collection.count()
+    print(f"Added {new_count - count} documents")
+
+if __name__ == "__main__":
+    document_name = "UU No.20 Tahun 2008 - UMKM"
+    document_description = "Undang-Undang tentang Usaha Mikro, Kecil, dan Menengah di Indonesia"
+    text = pdf_to_text(Utils.INDONESIA_ACT_URL)
+    embed_text_in_chromadb(text, document_name, document_description)
